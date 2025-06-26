38f1d3cfaddb30d4fb52ed24f9108f655d7b40e2
José Espinoza Bone <megamanx@outlook.com>
2025-06-25 23:24:38 -0500

    Add project files.

---
+++ InsaneIO.Xades/Signer/IXadesSigner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsaneIO.Xades.Signer
+{
+    public interface IXadesSigner
+    {
+        public string Sign();
+        public bool Verify();
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# InsaneIO.Xades
